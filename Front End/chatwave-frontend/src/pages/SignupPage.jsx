580050a6447af044b8cd7a775446fc0e5c591589
TejuDhumal <91212906+TejuDhumal@users.noreply.github.com>
2024-06-10 15:09:24 +0530

    contact and signup Page added

---
+++ Front End/chatwave-frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar"
+import Signup from "../components/Signup"
+import Footer from "../components/Footer"
+
+
+
+const SignupPage = () => {
+  return (
+    <>
+      <Navbar />
+      <Signup />
+      <Footer />
+    </>
+  )
+}
+
+export default SignupPage
