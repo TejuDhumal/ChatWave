ce5b3b97c0f927e6dd4185b5c2f32a0d0a9c5669
abhhishekguptaSE <138385407+abhhishekguptaSE@users.noreply.github.com>
2024-06-19 15:38:39 +0530

    Add files via upload

--- Front End/chatwave-frontend/src/App.jsx
+++ Front End/chatwave-frontend/src/App.jsx
@@ -1,27 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import SignupPage from './pages/SignupPage';
-import SigninPage from './pages/SigninPage';
-import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import DashboardPage from './pages/DashboardPage';
-
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import SignupPage from "./pages/SignupPage";
+import SigninPage from "./pages/SigninPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import DashboardPage from "./pages/DashboardPage";
+import ReactivateAccountPage from "./pages/ReactivateAccountPage";
+import CreateGroupRoom from "./components/CreateGroupRoom";
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-        <Route path='/about' element={<AboutPage />}></Route>
-        <Route path='/contact' element={<ContactPage />}></Route>
-        <Route path='/signup' element={<SignupPage />}></Route>
-        <Route path='/signin' element={<SigninPage />}></Route>
-        <Route path='/forgot-password' element={<ForgotPasswordPage />}></Route>
-        <Route path='/dashboard' element={<DashboardPage />}></Route>
+        <Route path="/group-room" element={<CreateGroupRoom />} />
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="/contact" element={<ContactPage />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="/signin" element={<SigninPage />}></Route>
+        <Route path="/forgot-password" element={<ForgotPasswordPage />}></Route>
+        <Route
+          path="/reactivate-account"
+          element={<ReactivateAccountPage />}
+        ></Route>
+        <Route path="/dashboard" element={<DashboardPage />}></Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
