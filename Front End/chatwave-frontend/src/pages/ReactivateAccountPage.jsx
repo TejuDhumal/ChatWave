ba431ef3f930091a215d43e4362ccfc243a5a381
Devaraju <114974220+devap1414@users.noreply.github.com>
2024-06-19 15:28:49 +0530

    Add files via upload
    Reactivate Account page

---
+++ Front End/chatwave-frontend/src/pages/ReactivateAccountPage.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import ReactivateAccount from "../components/ReactivateAccount";
+import Footer from "../components/Footer";
+
+const ReactivateAccountPage = () => {
+  return (
+    <>
+      <Navbar />
+      <ReactivateAccount />
+      <Footer />
+    </>
+  );
+};
+
+export default ReactivateAccountPage;
