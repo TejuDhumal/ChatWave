bbccfe8a32b6ab10b682bd36443df9f5d43b0bcb
TejuDhumal <91212906+TejuDhumal@users.noreply.github.com>
2024-06-24 16:53:37 +0530

    Delete Back End/usermanagement/chatwave-usermanagement-microservice directory

--- Back End/usermanagement/chatwave-usermanagement-microservice/src/main/java/com/axis/team4/codecrafters/user_service/config/SecurityConstant.java
+++
@@ -1,9 +0,0 @@
-// Set the JWT key and header for the security configuration.
-
-package com.axis.team4.codecrafters.user_service.config;
-
-public class SecurityConstant {
-  public static final String JWT_KEY =
-      "kzjjbeiurbZGyurZvzpaqekmeecfeeljliuogcerwmqzsduphbeheb";
-  public static final String HEADER = "Authorization";
-}
