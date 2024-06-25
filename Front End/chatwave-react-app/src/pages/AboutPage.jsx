529618d9d49e0a147a51b5d4c21f948df87e6920
TejuDhumal <91212906+TejuDhumal@users.noreply.github.com>
2024-06-25 10:03:34 +0530

    updated about page

---
+++ Front End/chatwave-react-app/src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+import Footer from "../components/Footer";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Footer />
+    </>
+  );
+};
+
+export default AboutPage;
