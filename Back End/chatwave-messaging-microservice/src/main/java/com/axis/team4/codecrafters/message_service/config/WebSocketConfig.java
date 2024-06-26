699f93fa07e0f3eec2d94b30c0dc175ac864ca7d
Naga Nithin <yuvanaganithin@gmail.com>
2024-06-26 19:36:57 +0530

    Add files via upload

---
+++ Back End/chatwave-messaging-microservice/src/main/java/com/axis/team4/codecrafters/message_service/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+// This is the configuration file for the WebSocket. It is used to configure the WebSocket for the application. It enables the WebSocket message broker and registers the Stomp endpoints.
+
+package com.axis.team4.codecrafters.message_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket.io")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+}
+
+
